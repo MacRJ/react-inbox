b887157fbab0656598a92783faa9554ffb492ed6
MacRJ <rmacwj@gmail.com>
2017-09-18 14:18:07 -0700

    the componets are displying with css

---
+++ src/components/Message.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from '../index.css'
+
+
+const Message = () => {
+
+return (
+  <div className="row message unread">
+    <div className="col-xs-1">
+      <div className="row">
+        <div className="col-xs-2">
+          <input type="checkbox" />
+        </div>
+        <div className="col-xs-2">
+          <i className="star fa fa-star-o"></i>
+        </div>
+      </div>
+    </div>
+    <div className="col-xs-11">
+      <a href="#">
+        Here is some message text that has a bunch of stuff
+      </a>
+    </div>
+  </div>
+
+)
+}
+
+export default Message
---
+++ src/components/MessageList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Message from './Message'
+import styles from '../index.css'
+
+
+class MessageList extends React.Component {
+  render() {
+    return (
+      <Message />
+  )
+  }
+}
+
+export default MessageList
